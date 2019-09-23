94af299e0bdb9f5fab5c5b5c870c2bea1c7a4bb6
Lewis <43108320+notthelewis@users.noreply.github.com>
2019-09-23 19:40:49 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,11 +3,11 @@
 dn=$(echo $1 | cut -d/ -f1 --complement | cut -d/ -f3 --complement | sed 's/\///g')
 
 if [ $2 == 0 ] || [ $2 == 2 ]; then 
-  ./whois.sh $dn > ../log/$dn.html && printf "\n" && ./dig.sh $dn >> ../log/$dn.html
-  cat ../log/$dn.html
+  ./whois.sh $dn > ./log/$dn.html && printf "\n" && ./dig.sh $dn >> ./log/$dn.html
+  cat ./log/$dn.html
 fi
 
 if [ $2 == 1 ] || [ $2 == 3 ]; then
-  ./whois.sh $dn > ../log/$dn.html && ./digX.sh $dn >> ../log/$dn.html
-  cat ../log/$dn.html|sed 's/has address/A/g' | sed 's/has IPv6 address/AAAA/g'| sed 's/descriptive text/TXT/g' | sed 's/mail is handled by/MX/g' | sed 's/name server/NS/g' | sed 's/has SOA record/SOA/g'
+  ./whois.sh $dn > ./log/$dn.html && ./digX.sh $dn >> ./log/$dn.html
+  cat ./log/$dn.html|sed 's/has address/A/g' | sed 's/has IPv6 address/AAAA/g'| sed 's/descriptive text/TXT/g' | sed 's/mail is handled by/MX/g' | sed 's/name server/NS/g' | sed 's/has SOA record/SOA/g'
 fi
